04e8948e0ac7f73806012405afab228bd24d6466
av-dev2 <emgani@aakvatech.com>
2024-09-11 21:55:42 +0300

    chore: rename 'Ship Manifest' to 'Manifest'

--- icd_tz/icd_tz/doctype/ship_manifest/ship_manifest.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Nelson Mpanju and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Ship Manifest', {
-	// refresh: function(frm) {
-
-	// }
-});
--- icd_tz/icd_tz/doctype/ship_manifest/test_ship_manifest.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Nelson Mpanju and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestShipManifest(FrappeTestCase):
-	pass
