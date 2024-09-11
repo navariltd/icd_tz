312c6d143334fa42eb4a14efd1e9cbf3fbe9ad37
av-dev2 <emgani@aakvatech.com>
2024-09-11 23:42:44 +0300

    feat: extract data from manifest excel file

--- icd_tz/icd_tz/doctype/manifest/manifest.js
+++ icd_tz/icd_tz/doctype/manifest/manifest.js
@@ -2,7 +2,25 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Manifest', {
-	// refresh: function(frm) {
+	refresh: (frm) => {
 
-	// },
+	},
+	manifest: (frm) => {
+		if (frm.doc.manifest) {
+			frappe.call({
+				method: "extract_data_from_manifest_file",
+				doc: frm.doc,
+				args: {
+				},
+				freeze: true,
+				freeze_message: __("Extracting data from manifest file..."),
+				callback: (r) => {
+					if (r.message) {
+						frm.refresh();
+					}
+				}
+			});
+			console.log(frm.doc.manifest);
+		}
+	},
 });
