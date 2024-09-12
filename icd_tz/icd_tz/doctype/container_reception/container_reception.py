8a3a84dbf0c92df0aa4755cba0e0a1acbf4105de
av-dev2 <emgani@aakvatech.com>
2024-09-12 18:04:34 +0300

    feat: validate duplicates of CR based on selected Movement order

--- icd_tz/icd_tz/doctype/container_reception/container_reception.py
+++ icd_tz/icd_tz/doctype/container_reception/container_reception.py
@@ -1,8 +1,34 @@
 # Copyright (c) 2024, Nathan Kagoro and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.query_builder import DocType
+from frappe.utils import get_link_to_form
 from frappe.model.document import Document
 
+cr = DocType("Container Reception")
+
 class ContainerReception(Document):
-	pass
+	def validate(self):
+		self.validate_duplicate_cmo()
+
+	def validate_duplicate_cmo(self):
+		"""Validate that there is no duplicate Container Reception based on Container Movement Order (CMO)"""
+		if self.movement_order:
+			duplicates = (
+				frappe.qb.from_(cr)
+				.select(
+					cr.name
+				)
+				.where(
+					(cr.movement_order == self.movement_order)
+					& (cr.name != self.name)
+				)
+			).run(as_dict=True)
+
+			if len(duplicates) > 0:
+				url = get_link_to_form("Container Reception", duplicates[0].name)
+				frappe.throw(
+					f"Another Container Reception with the same Movement Order already exists: <a href='{url}'><b>{duplicates[0].name}</b></a>"
+				)
+
