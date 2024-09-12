28fb6820899d8ea7aed2ba6f57e3c74360098d14
av-dev2 <emgani@aakvatech.com>
2024-09-12 13:16:46 +0300

    feat: validate duplication of CMO per single container and validate if container is found on the selected manifest

--- icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.js
+++ icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2024, Nathan Kagoro and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Container Movement Order", {
-// 	refresh(frm) {
+frappe.ui.form.on("Container Movement Order", {
+	refresh: (frm) => {
+        frm.trigger("set_queries");
 
-// 	},
-// });
+	},
+    onload: (frm) => {
+        frm.trigger("set_queries");
+    },
+    set_queries: (frm) => {
+        frm.set_query("manifest", () => {
+            return {
+                filters: {
+                    docstatus: 1
+                }
+            };
+        });
+    },
+
--- icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.py
+++ icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.py
@@ -1,9 +1,48 @@
 # Copyright (c) 2024, Nathan Kagoro and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.utils import get_url_to_form
+from frappe.query_builder import DocType
 from frappe.model.document import Document
 
+cmo = DocType("Container Movement Order")
 
 class ContainerMovementOrder(Document):
-	pass
+	def validate(self):
+		if self.container_number:
+			self.validate_container_is_in_manifest()
+			self.validate_duplicate_cmo_per_container_number()
+	
+	
+	def validate_container_is_in_manifest(self):
+		"""Validate that the container number is in the selected manifest"""
+
+		if not frappe.db.exists(
+			"Containers Detail",
+			{"parent": self.manifest, "container_no": self.container_number}
+		):
+			frappe.throw(
+				f"Container: {self.container_number} does not belong to the this Manifest: {self.manifest}"
+			)
+	
+	def validate_duplicate_cmo_per_container_number(self):
+		"""Validate the duplicate of cmo per container number"""
+		duplicates = (
+			frappe.qb.from_(cmo)
+			.select(
+				cmo.name.as_(cmo_id)
+			)
+			.where(
+				(cmo.manifest == self.manifest) &
+				(cmo.container_number == self.container_number)
+				(cmo.name != self.name)
+			)
+		).run(as_dict=True)
+		if duplicates:
+			url = get_url_to_form("Container Movement Order", duplicates[0].cmo_id)
+			frappe.throw(
+				f"Container: {self.container_number} already has a Movement Order: <a href='{url}'>{duplicates[0].cmo_id}</a>"
+			)
+	
+				
