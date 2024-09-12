981c383b9818fe79a4d24e55f4dc37cfe5b10d4c
av-dev2 <emgani@aakvatech.com>
2024-09-12 15:58:00 +0300

    feat: fetch information from CMO to Container Reception

--- icd_tz/icd_tz/doctype/container_reception/container_reception.js
+++ icd_tz/icd_tz/doctype/container_reception/container_reception.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Container Reception', {
-	// refresh: function(frm) {
+	refresh: (frm) => {
 
-	// }
+	},
+	onload: (frm) => {
+		frm.trigger("set_queries");
+	},
+    set_queries: (frm) => {
+        frm.set_query("movement_order", () => {
+            return {
+                filters: {
+                    docstatus: 1
+                }
+            };
+        });
+    },
 });
