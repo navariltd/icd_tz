6d2601cdb8a164f7babae1ce6f88e346cb2fd3b7
av-dev2 <emgani@aakvatech.com>
2024-09-12 18:54:19 +0300

    feat: create received container on submit of container reception

--- icd_tz/icd_tz/doctype/container_reception/container_reception.py
+++ icd_tz/icd_tz/doctype/container_reception/container_reception.py
@@ -3,14 +3,17 @@
 
 import frappe
 from frappe.query_builder import DocType
-from frappe.utils import get_link_to_form
 from frappe.model.document import Document
+from frappe.utils import get_link_to_form, nowdate
 
 cr = DocType("Container Reception")
 
 class ContainerReception(Document):
 	def validate(self):
 		self.validate_duplicate_cmo()
+	
+	def on_submit(self):
+		self.create_container()
 
 	def validate_duplicate_cmo(self):
 		"""Validate that there is no duplicate Container Reception based on Container Movement Order (CMO)"""
@@ -32,3 +35,20 @@ class ContainerReception(Document):
 					f"Another Container Reception with the same Movement Order already exists: <a href='{url}'><b>{duplicates[0].name}</b></a>"
 				)
 
+	def create_container(self):
+		"""Create a Container record from the Container Reception"""
+
+		container = frappe.new_doc("Container")
+		container.container_reception = self.name
+		container.container_no = self.container_no
+		container.size = self.size
+		container.volume = self.cbm
+		container.weght = self.weight
+		container.seal_no_1 = self.seal_no_1
+		container.seal_no_2 = self.seal_no_2
+		container.port_of_origin = self.port
+		container.port_of_destination = self.port
+		container.arrival_date = nowdate()
+		container.save(ignore_permissions=True)
+
+		return container.name
