3ce8ed3bf069632f560747449e08a44f8adedb98
av-dev2 <emgani@aakvatech.com>
2024-09-13 15:15:42 +0300

    feat: icd tz settings

---
+++ icd_tz/icd_tz/doctype/icd_tz_settings/icd_tz_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Nathan Kagoro and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ICD TZ Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
