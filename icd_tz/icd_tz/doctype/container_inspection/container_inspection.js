4824258a610d1c51387eb973a24a167f4966e4b6
av-dev2 <emgani@aakvatech.com>
2024-09-13 15:14:41 +0300

    chore: set company and set filter of services by 'ICD Services' as Item Group

--- icd_tz/icd_tz/doctype/container_inspection/container_inspection.js
+++ icd_tz/icd_tz/doctype/container_inspection/container_inspection.js
@@ -2,7 +2,19 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Container Inspection', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: (frm) =>{
+		frm.trigger("set_filters");
+	},
+	onload: (frm) => {
+		frm.trigger("set_filters");
+	},
+	set_filters: (frm) => {
+		frm.set_query("service", "services", () => {
+			return {
+				filters: {
+					"item_group": "ICD Services"
+				}
+			};
+		});
+	},
 });
