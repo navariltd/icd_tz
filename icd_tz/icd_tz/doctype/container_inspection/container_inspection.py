c4c010015fb98dde4ffb65cac98adb473e84eff2
av-dev2 <emgani@aakvatech.com>
2024-09-13 12:10:25 +0300

    feat: container inspection

---
+++ icd_tz/icd_tz/doctype/container_inspection/container_inspection.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Nathan Kagoro and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class ContainerInspection(Document):
+	pass
