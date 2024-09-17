252ea6d97298104ae231578cd284b9a65d087277
av-dev2 <emgani@aakvatech.com>
2024-09-17 15:26:07 +0300

    feat: add in yard booking item and storage item for billing purposes

--- icd_tz/icd_tz/doctype/icd_tz_settings/icd_tz_settings.js
+++ icd_tz/icd_tz/doctype/icd_tz_settings/icd_tz_settings.js
@@ -2,7 +2,26 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('ICD TZ Settings', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: (frm) => {
+		frm.trigger('set_filters');
+	},
+	onload: (frm) => {
+		frm.trigger('set_filters');
+	},
+	set_filters: (frm) => {
+		frm.set_query("in_yard_booking_item", () => {
+			return {
+				filters: {
+					"item_group": "ICD Services"
+				}
+			};
+		});
+		frm.set_query("container_storage_item", () => {
+			return {
+				filters: {
+					"item_group": "ICD Services"
+				}
+			};
+		});
+	}
 });
