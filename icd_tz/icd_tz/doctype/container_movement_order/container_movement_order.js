d6eb73cc3f369e8bce4abd764fddff37daac9e09
av-dev2 <emgani@aakvatech.com>
2024-09-17 09:12:35 +0300

    feat: get container size from manifest

--- icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.js
+++ icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.js
@@ -35,11 +35,29 @@ frappe.ui.form.on("Container Movement Order", {
                 callback: (r) => {
                     if (r.message) {
                         let data = r.message;
-                        frm.set_value("vessel_name", data.voyage);
+                        frm.set_value("ship", data.vessel_name);
+                        frm.set_value("company", data.company);
                         frm.set_value("received_date", data.arrival_date);
-                        frm.set_value("voyage_no", data.voyage);
-                        frm.set_df_property("container_number", "options", data.containers);
+                        frm.set_value("voyage_no", data.voyage_no);
+                        frm.set_df_property("container_no", "options", data.containers);
                         frm.refresh_fields();
+                    }
+                }
+            });
+        }
+    },
+    container_no: (frm) => {
+        if (frm.doc.manifest && frm.doc.container_no) {
+            frappe.call({
+                method: "icd_tz.icd_tz.doctype.container_movement_order.container_movement_order.get_container_size_from_manifest",
+                args: {
+                    manifest: frm.doc.manifest,
+                    container_no: frm.doc.container_no
+                },
+                callback: (r) => {
+                    if (r.message) {
+                        frm.set_value("size", r.message);
+                        frm.refresh_field("size");
                     }
                 }
             });
@@ -51,12 +69,11 @@ frappe.ui.form.on("Container Movement Order", {
                 frappe.new_doc('Container Reception', {
                     "movement_order": frm.doc.name,
                     "manifest": frm.doc.manifest,
-                    "ship_name": frm.doc.ship,
-                    "vessel_name": frm.doc.vessel_name,
+                    "ship": frm.doc.ship,
                     "received_date": frm.doc.received_date,
                     "voyage_no": frm.doc.voyage_no,
                     "port": frm.doc.port,
-                    "container_no": frm.doc.container_number,
+                    "container_no": frm.doc.container_no,
                     "size": frm.doc.size,
                     "truck_name": frm.doc.truck_name,
                 }, doc => {});
