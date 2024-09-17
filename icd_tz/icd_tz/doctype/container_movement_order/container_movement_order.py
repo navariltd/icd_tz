0ba9d7dc2e18f71b0eceb49ddc22cd660926e867
av-dev2 <emgani@aakvatech.com>
2024-09-17 09:13:24 +0300

    feat: get container size from manifest

--- icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.py
+++ icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.py
@@ -2,9 +2,9 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.utils import get_url_to_form
 from frappe.query_builder import DocType
 from frappe.model.document import Document
+from frappe.utils import get_url_to_form, cint
 
 cmo = DocType("Container Movement Order")
 
@@ -14,7 +14,7 @@ class ContainerMovementOrder(Document):
 			self.company = frappe.defaults.get_user_default("Company")
 		
 	def validate(self):
-		if self.container_number:
+		if self.container_no:
 			self.validate_container_is_in_manifest()
 			self.validate_duplicate_cmo_per_container_number()
 
@@ -26,10 +26,10 @@ class ContainerMovementOrder(Document):
 
 		if not frappe.db.exists(
 			"Containers Detail",
-			{"parent": self.manifest, "container_no": self.container_number}
+			{"parent": self.manifest, "container_no": self.container_np}
 		):
 			frappe.throw(
-				f"Container: {self.container_number} does not belong to the this Manifest: {self.manifest}"
+				f"Container: {self.container_no} does not belong to the this Manifest: {self.manifest}"
 			)
 	
 	def validate_duplicate_cmo_per_container_number(self):
@@ -41,22 +41,22 @@ class ContainerMovementOrder(Document):
 			)
 			.where(
 				(cmo.manifest == self.manifest) &
-				(cmo.container_number == self.container_number)
+				(cmo.container_no == self.container_no)
 				& (cmo.name != self.name)
 			)
 		).run(as_dict=True)
 		if duplicates:
 			url = get_url_to_form("Container Movement Order", duplicates[0].cmo_id)
 			frappe.throw(
-				f"Container: {self.container_number} already has a Movement Order: <a href='{url}'>{duplicates[0].cmo_id}</a>"
+				f"Container: {self.container_no} already has a Movement Order: <a href='{url}'>{duplicates[0].cmo_id}</a>"
 			)
 	
 	def update_container_has_order(self):
 		"""Update the status of the container to Has Order"""
-		if self.container_number:
+		if self.container_no:
 			frappe.db.set_value(
 				"Containers Detail",
-				{"parent": self.manifest, "container_no": self.container_number},
+				{"parent": self.manifest, "container_no": self.container_no},
 				"has_order", 1
 			)
 
@@ -68,7 +68,7 @@ def get_manifest_details(manifest):
 	details = frappe.db.get_value(
 		"Manifest",
 		manifest,
-		["mrn", "vessel_name", "tpa_uid", "voyage", "arrival_date", "departure_date", "call_sign"], 
+		["mrn", "vessel_name", "tpa_uid", "voyage_no", "arrival_date", "departure_date", "call_sign", "company"], 
 		as_dict=True
 	)
 
@@ -83,3 +83,14 @@ def get_manifest_details(manifest):
 		details["containers"] = containers
 
 	return details
+
+@frappe.whitelist()
+def get_container_size_from_manifest(manifest, container_no):
+	"""Get the size of the container from the manifest"""
+	
+	size = frappe.db.get_value(
+		"Containers Detail",
+		{"parent": manifest, "container_no": container_no},
+		"container_size"
+	)
+	return size
