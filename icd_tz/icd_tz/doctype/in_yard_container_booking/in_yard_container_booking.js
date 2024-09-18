c3a422205891871fa8b43519e19bb1d04a3dc296
av-dev2 <emgani@aakvatech.com>
2024-09-18 18:58:14 +0300

    feat: add cf company in yard container booking and validate selected cf agent if belong to cf company

--- icd_tz/icd_tz/doctype/in_yard_container_booking/in_yard_container_booking.js
+++ icd_tz/icd_tz/doctype/in_yard_container_booking/in_yard_container_booking.js
@@ -3,17 +3,36 @@
 
 frappe.ui.form.on("In Yard Container Booking", {
 	refresh(frm) {
+		frm.trigger("set_filters");
         frm.trigger("create_container_inspection");
 	},
     onload: (frm) => {
         if (!frm.doc.company) {
             frm.set_value("company", frappe.defaults.get_user_default("Company"));
         }
+		frm.trigger("set_filters");
         
         frm.trigger("create_container_inspection");
     },
+	set_filters: (frm) => {
+		frm.set_query("c_and_f_company", () => {
+			return {
+				filters: {
+					"disabled": 0
+				}
+			};
+		});
+		frm.set_query("c_and_f_agent", () => {
+			return {
+				filters: {
+					"disabled": 0,
+					"c_and_f_company": frm.doc.c_and_f_company
+				}
+			};
+		});
+	},
 	create_container_inspection: (frm) => {
-		// if (frm.doc.docstatus == 1) {
+		if (frm.doc.docstatus == 1) {
 			frm.add_custom_button(__('Create Container Inspection'), () => {
 				frappe.new_doc('Container Inspection', {
 					"in_yard_container_booking": frm.doc.name,
@@ -23,6 +42,6 @@ frappe.ui.form.on("In Yard Container Booking", {
                     "inspection_date": frm.doc.booking_date + " " + frm.doc.booking_time,
 				}, doc => {});
 			}).addClass('btn-primary');
-		// }
+		}
 	}
 });
--- icd_tz/icd_tz/doctype/in_yard_container_booking/in_yard_container_booking.py
+++ icd_tz/icd_tz/doctype/in_yard_container_booking/in_yard_container_booking.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, Nathan Kagoro and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -9,3 +9,15 @@ class InYardContainerBooking(Document):
 	def before_save(self):
 		if not self.company:
 			self.company = frappe.defaults.get_user_default("Company")
+		
+	def validate(self):
+		self.validate_cf_agent()
+
+	def validate_cf_agent(self):
+		"""
+		Validate the Clearing and Forwarding Agent
+		"""
+		if self.c_and_f_agent:
+			cf_company = frappe.get_cached_value("Clearing Agent", self.c_and_f_agent, "c_and_f_company")
+			if self.c_and_f_company != cf_company:
+				frappe.throw(f"The selected Clearing Agent: <b>{self.c_and_f_agent}</b> does not belong to the selected Clearing and Forwarding Company: <b>{self.c_and_f_company}</b>")
