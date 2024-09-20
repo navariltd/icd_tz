256dfb1c04e1acf6aeff19870fdb3759a83a495d
av-dev2 <emgani@aakvatech.com>
2024-09-20 11:53:22 +0300

    feat: validate cf agent if the agent is under cf company

--- icd_tz/icd_tz/doctype/container_inspection/container_inspection.py
+++ icd_tz/icd_tz/doctype/container_inspection/container_inspection.py
@@ -6,6 +6,22 @@ from frappe.utils import nowdate
 from frappe.model.document import Document
 
 class ContainerInspection(Document):
+    def before_save(self):
+        if not self.company:
+            self.company = frappe.defaults.get_user_default("Company")
+    
+    def validate(self):
+        self.validate_cf_agent()
+    
+    def validate_cf_agent(self):
+        """
+		Validate the Clearing and Forwarding Agent
+		"""
+        if self.c_and_f_company and self.c_and_f_agent:
+            cf_company = frappe.get_cached_value("Clearing Agent", self.c_and_f_agent, "c_and_f_company")
+            if self.c_and_f_company != cf_company:
+                frappe.throw(f"The selected Clearing Agent: <b>{self.c_and_f_agent}</b> does not belong to the selected Clearing and Forwarding Company: <b>{self.c_and_f_company}</b>")
+
     @frappe.whitelist()
     def get_strip_services(self):
         if isinstance(self, str):
