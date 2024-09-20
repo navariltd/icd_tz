720fa1369eb1eaefcc97d1c836503b50770a656c
av-dev2 <emgani@aakvatech.com>
2024-09-20 12:28:22 +0300

    feat: make sales ordr from service order

--- icd_tz/icd_tz/doctype/container_inspection/container_inspection.js
+++ icd_tz/icd_tz/doctype/container_inspection/container_inspection.js
@@ -4,7 +4,6 @@
 frappe.ui.form.on('Container Inspection', {
 	refresh: (frm) =>{
 		frm.trigger("set_filters");
-		frm.trigger("make_sales_order");
 	},
 	onload: (frm) => {
 		frm.trigger("set_filters");
@@ -17,31 +16,5 @@ frappe.ui.form.on('Container Inspection', {
 				}
 			};
 		});
-	},
-	make_sales_order: (frm) => {
-		frm.add_custom_button(__("Make Sales Order"), () => {
-			if (!frm.doc.customer) {
-				frappe.msgprint("Please select a customer");
-				return;
-			}
-			if (!frm.doc.services.length) {
-				frappe.msgprint("Please add services to the inspection");
-				return;
-			}
-			frappe.call({
-				method: "icd_tz.icd_tz.api.sales_order.make_sales_order",
-				args: {
-					doc_type: frm.doc.doctype,
-					doc_name: frm.doc.name
-				},
-				freeze: true,
-				freeze_message: __("Creating Sales Order"),
-				callback: (r) => {
-					if (r.message) {
-						frappe.set_route("Form", "Sales Order", r.message);
-					}
-				}
-			});
-		}).addClass("btn-primary");
 	},
 });
--- icd_tz/icd_tz/doctype/service_order/service_order.js
+++ icd_tz/icd_tz/doctype/service_order/service_order.js
@@ -4,6 +4,7 @@
 frappe.ui.form.on('Service Order', {
 	refresh: (frm) =>{
 		frm.trigger("set_filters");
+		frm.trigger("make_sales_order");
 	},
 	onload: (frm) => {
 		frm.trigger("set_filters");
@@ -57,6 +58,30 @@ frappe.ui.form.on('Service Order', {
 					}
 				}
 			});
+		}
+	},
+	make_sales_order: (frm) => {
+		if (!frm.doc.sales_order && frm.doc.docstatus == 1) {
+			frm.add_custom_button(__("Make Sales Order"), () => {
+				if (!frm.doc.services.length) {
+					frappe.msgprint("Please add services to the inspection");
+					return;
+				}
+				frappe.call({
+					method: "icd_tz.icd_tz.api.sales_order.make_sales_order",
+					args: {
+						doc_type: frm.doc.doctype,
+						doc_name: frm.doc.name
+					},
+					freeze: true,
+					freeze_message: __("Creating Sales Order"),
+					callback: (r) => {
+						if (r.message) {
+							frappe.set_route("Form", "Sales Order", r.message);
+						}
+					}
+				});
+			}).addClass("btn-primary");
 		}
 	},
 });
