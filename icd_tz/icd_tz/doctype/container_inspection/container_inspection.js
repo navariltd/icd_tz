e52deccee145cf736fa0c1656c4cc14c78ec3f33
av-dev2 <emgani@aakvatech.com>
2024-09-20 12:44:37 +0300

    feat: create service order directly from container inspection

--- icd_tz/icd_tz/doctype/container_inspection/container_inspection.js
+++ icd_tz/icd_tz/doctype/container_inspection/container_inspection.js
@@ -4,9 +4,11 @@
 frappe.ui.form.on('Container Inspection', {
 	refresh: (frm) =>{
 		frm.trigger("set_filters");
+		frm.trigger("create_service_order");
 	},
 	onload: (frm) => {
 		frm.trigger("set_filters");
+		frm.trigger("create_service_order");
 	},
 	set_filters: (frm) => {
 		frm.set_query("service", "services", () => {
@@ -17,4 +19,25 @@ frappe.ui.form.on('Container Inspection', {
 			};
 		});
 	},
+	create_service_order: (frm) => {
+		if (!frm.doc.service_order & frm.doc.docstatus == 1) {
+			frm.add_custom_button(__('Create Service Order'), () => {
+				frappe.new_doc('Service Order', {
+					"container_inspection": frm.doc.name,
+					"customer": frm.doc.customer,
+					"clearing_agent": frm.doc.c_and_f_agent,
+					"c_and_f_company": frm.doc.c_and_f_company,
+					"container_no": frm.doc.container_no,
+					"container_location": frm.doc.container_location,
+				}, doc => {
+					if (frm.doc.services.length > 0) {
+						frm.doc.services.forEach((row) => {
+							let new_row = frappe.model.add_child(doc, "services");
+							new_row.service = row.service;
+						});
+					}
+				});
+			}).addClass('btn-primary');
+		}
+	}
 });
