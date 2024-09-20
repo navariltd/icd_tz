4693fb1f57b47d81d89000cdb9f94e7df9a41c76
av-dev2 <emgani@aakvatech.com>
2024-09-20 11:56:20 +0300

    feat: service order and service order detail for showing items ready for billing

---
+++ icd_tz/icd_tz/doctype/service_order/service_order.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Nathan Kagoro and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Service Order', {
+	// refresh: function(frm) {
+
+	// }
+});
