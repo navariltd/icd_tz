0c6cd730a31c167ec8f842061ce93c684ce43d77
av-dev2 <emgani@aakvatech.com>
2024-10-02 22:30:49 +0300

    feat: validate storage days and destination details

--- icd_tz/icd_tz/doctype/icd_tz_settings/icd_tz_settings.py
+++ icd_tz/icd_tz/doctype/icd_tz_settings/icd_tz_settings.py
@@ -5,4 +5,18 @@
 from frappe.model.document import Document
 
 class ICDTZSettings(Document):
-	pass
+	def before_save(self):
+		self.validate_storage_days()
+	
+	def validate_storage_days(self):
+		storage_days = []
+		for row in self.storage_days:
+			d = {
+				"destination": row.destination,
+				"charge": row.charge
+			}
+			if d in storage_days:
+				frappe.throw(f"At Row#: {row.idx}, {row.destination} with charge {row.charge} already exists")
+			else:
+				storage_days.append(d)
+
