36ee7feca019b40ed5c73456926ac0204a6eb1c3
av-dev2 <emgani@aakvatech.com>
2025-01-06 17:25:48 +0300

    feat: enhance sales order creation by adding support for multiple service orders using M BL No and manifest

--- icd_tz/icd_tz/api/sales_order.py
+++ icd_tz/icd_tz/api/sales_order.py
@@ -18,44 +18,60 @@ def unlink_sales_order(doc):
     service_order.reload()
 
 @frappe.whitelist()
-def make_sales_order(doc_type, doc_name):
-    doc = frappe.get_doc(doc_type, doc_name)
-    settings_doc = frappe.get_doc("ICD TZ Settings")
-    container_doc = frappe.get_doc("Container", doc.get("container_id"))
+def make_sales_order(doc_type, doc_name, m_bl_no=None, manifest=None):
+    service_docs = []
+
+    if m_bl_no and manifest:
+        service_docs = get_orders(m_bl_no, manifest)
+    
+    if len(service_docs) == 0:
+        if not doc_type or not doc_name:
+            return
+        
+        source_doc = frappe.get_doc(doc_type, doc_name)
+        service_docs.append(source_doc)
+
 
     items = []
-    single_days, double_days = get_container_days_to_be_billed(doc, container_doc, settings_doc)
-    for item in doc.get("services"):
-        qty = 1
-        container_childs = ""
-        if item.get("service") in [settings_doc.get("storage_item_single_20ft"), settings_doc.get("storage_item_single_40ft")]:
-            qty = len(single_days)
-            container_childs = ",".join(single_days)
-        
-        elif item.get("service") in [settings_doc.get("storage_item_double_20ft"), settings_doc.get("storage_item_double_40ft")]:
-            qty = len(double_days)
-            container_childs = ",".join(double_days)
-            
-        row_item = {
-            'item_code': item.get("service"),
-            'qty': qty,
-        }
-        if len(container_childs) > 0:
-            row_item["container_child_refs"] = container_childs
-        
-        items.append(row_item)
+    company = None
+    consignee = None
+    c_and_f_company = None
+    order_m_bl_no = m_bl_no if m_bl_no else None
+    order_manifest = manifest if manifest else None
 
+    settings_doc = frappe.get_cached_doc("ICD TZ Settings")
+    
+    for doc in service_docs:
+        container_doc = frappe.get_doc("Container", doc.get("container_id"))
+
+        single_days, double_days = get_container_days_to_be_billed(doc, container_doc, settings_doc)
+        items += get_items(doc, single_days, double_days, settings_doc)
+
+        if not consignee:
+            consignee = doc.consignee
+        
+        if not company:
+            company = doc.company
+        
+        if not c_and_f_company:
+            c_and_f_company = doc.c_and_f_company
+        
+        if not order_m_bl_no:
+            order_m_bl_no = doc.m_bl_no
+        
+        if not order_manifest:
+            order_manifest = doc.manifest
+    
     sales_order = frappe.get_doc({
         "doctype": "Sales Order",
-        "company": doc.get("company"),
-        "customer": doc.c_and_f_company,
+        "company": company,
+        "customer": c_and_f_company,
         "transaction_date": nowdate(),
         "delivery_date": nowdate(),
         "selling_price_list": settings_doc.get("default_price_list"),
         "items": items,
-        "container_no": doc.container_no,
-        "service_order": doc.name,
-        "consignee": doc.consignee,
+        "consignee": consignee,
+        "m_bl_no": order_m_bl_no,
     })
     
     sales_order.insert()
@@ -64,9 +80,12 @@ def make_sales_order(doc_type, doc_name):
     sales_order.save(ignore_permissions=True)
     sales_order.reload()
 
-    doc.db_set("sales_order", sales_order.name)
+    for doc in service_docs:
+        doc.db_set("sales_order", sales_order.name)
+    
     frappe.msgprint(f"Sales Order <b>{sales_order.name}</b> created successfully", alert=True)
     return sales_order.name
+
 
 def get_container_days_to_be_billed(service_doc, container_doc, settings_doc):
     single_days = []
@@ -82,10 +101,10 @@ def get_container_days_to_be_billed(service_doc, container_doc, settings_doc):
     for d in settings_doc.storage_days:
         if d.destination == service_doc.place_of_destination:
             if d.charge == "Single":
-                no_of_single_days = d.get("to") - d.get("from")
+                no_of_single_days = d.get("to") - d.get("from") + 1
 
             elif d.charge == "Double":
-                no_of_double_days = d.get("to") - d.get("from")
+                no_of_double_days = d.get("to") - d.get("from") + 1
                 
     for row in container_doc.container_dates:
         if (
@@ -100,7 +119,7 @@ def get_container_days_to_be_billed(service_doc, container_doc, settings_doc):
             row.is_billable == 1 and
             container_doc.has_double_charge == 1 and
             single_charge_count >= no_of_single_days and
-            double_charge_count < no_of_double_days
+            double_charge_count <= no_of_double_days
         ):
             double_days.append(row)
             double_charge_count += 1
@@ -108,3 +127,51 @@ def get_container_days_to_be_billed(service_doc, container_doc, settings_doc):
     single_days = [row.name for row in single_days if not row.sales_invoice]
     double_days = [row.name for row in double_days if not row.sales_invoice]
     return single_days, double_days
+
+
+def get_items(doc, single_days, double_days, settings_doc):
+    items = []
+    for item in doc.get("services"):
+        qty = 1
+        container_childs = ""
+        if item.get("service") in [settings_doc.get("storage_item_single_20ft"), settings_doc.get("storage_item_single_40ft")]:
+            qty = len(single_days)
+            container_childs = ",".join(single_days)
+        
+        elif item.get("service") in [settings_doc.get("storage_item_double_20ft"), settings_doc.get("storage_item_double_40ft")]:
+            qty = len(double_days)
+            container_childs = ",".join(double_days)
+        
+        row_item = {
+            'item_code': item.get("service"),
+            'qty': qty,
+            'container_no': doc.container_no,
+            'container_id': doc.container_id
+        }
+        if container_childs:
+            row_item["container_child_refs"] = container_childs
+        
+        items.append(row_item)
+    
+    return items
+
+
+def get_orders(m_bl_no, manifest):
+    service_docs = []
+
+    filters = {
+        "manifest": manifest,
+        "m_bl_no": m_bl_no,
+        "docstatus": 1,
+    }
+    orders = frappe.db.get_all(
+        "Service Order",
+        filters=filters,
+    )
+
+    for entry in orders:
+        doc = frappe.get_doc("Service Order", entry.name)
+        service_docs.append(doc)
+
+    return service_docs
+    
