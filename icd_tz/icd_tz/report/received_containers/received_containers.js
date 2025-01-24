a56128a7a7b12dbbf591d5bb1f466c13839ddd7b
Emmafidelis <emanuelkagombora28@gmail.com>
2025-01-24 19:50:12 +0300

    feat: Add filters on received and exited reports

--- icd_tz/icd_tz/report/received_containers/received_containers.js
+++ icd_tz/icd_tz/report/received_containers/received_containers.js
@@ -5,40 +5,26 @@
 // Copyright (c) 2024, ICD TZ and contributors
 // For license information, please see license.txt
 
-// frappe.query_reports["Received Containers"] = {
-//     "filters": [
-//         {
-//             "fieldname": "from_date",
-//             "label": __("From Date"),
-//             "fieldtype": "Date",
-//             "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1)
-//         },
-//         {
-//             "fieldname": "to_date",
-//             "label": __("To Date"),
-//             "fieldtype": "Date",
-//             "default": frappe.datetime.get_today()
-//         },
-//         {
-//             "fieldname": "container_no",
-//             "label": __("Container No"),
-//             "fieldtype": "Data"
-//         },
-//         {
-//             "fieldname": "shipping_line",
-//             "label": __("Shipping Line"),
-//             "fieldtype": "Data"
-//         },
-//         {
-//             "fieldname": "vessel",
-//             "label": __("Vessel"),
-//             "fieldtype": "Data"
-//         },
-//         {
-//             "fieldname": "consignee",
-//             "label": __("Consignee"),
-//             "fieldtype": "Link",
-//             "options": "Consignee"
-//         }
-//     ]
-// };
+frappe.query_reports["Received Containers"] = {
+    "filters": [
+        {
+            "fieldname": "from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -1)
+        },
+        {
+            "fieldname": "to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today()
+        },
+        {
+            "fieldname": "bl_no",
+            "label": __("B/L No."),
+            "fieldtype": "Data",
+            
+        },
+        
+    ]
+};
