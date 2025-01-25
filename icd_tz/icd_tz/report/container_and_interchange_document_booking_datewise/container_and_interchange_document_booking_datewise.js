5f853f11ae786f5e0e1f2524f5d115d4ab215dad
Emanuel Kagombora <emanuelkagombora28@gmail.com>
2025-01-25 18:18:45 +0300

    feat: Container and Interchange Document Booking Datewise

---
+++ icd_tz/icd_tz/report/container_and_interchange_document_booking_datewise/container_and_interchange_document_booking_datewise.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, elius mgani and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Container and Interchange Document Booking Datewise"] = {
+	"filters": [
+
+	]
+};
