980afd40b8b8be324c812896f981a65b5f8a6672
av-dev2 <emgani@aakvatech.com>
2025-01-27 16:40:41 +0300

    feat: implement submit button functionality for Service Orders in list view

--- icd_tz/icd_tz/doctype/service_order/service_order_list.js
+++ icd_tz/icd_tz/doctype/service_order/service_order_list.js
@@ -4,17 +4,44 @@ frappe.listview_settings["Service Order"] = {
 
     onload: (listview) => {
         listview.page.add_inner_button(__("Create Bulk Service Order"), () => {
-            service_order_show_dialog(listview);
+            show_dialog(listview);
         }).removeClass("btn-default").addClass("btn-info btn-sm");
-
-        listview.page.add_inner_button(__("Create Sales Order"), () => {
-            sales_order_show_dialog();
-        }).removeClass("btn-default").addClass("btn-warning btn-sm");
-    }
+    },
+    button: {
+        show: function(doc) {
+            return doc.docstatus === 0;
+        },
+        get_label: function() {
+          return __("<strong>Submit</strong>");
+        },
+        get_description: function(doc) {
+          return __("Submit {0}", [
+            `${__(doc.name)}: ${doc.container_no}`
+          ]);
+        },
+        action: function(doc) {
+            frappe.call({
+                method: 'icd_tz.icd_tz.api.utils.submit_doc',
+                args: {
+                    doc_type: 'Service Order',
+                    doc_name: doc.name
+                },
+                freeze: true,
+                freeze_message: __('<i class="fa fa-spinner fa-spin fa-4x"></i>'),
+                callback: function(response) {
+                    if (response.message) {
+                        frappe.show_alert(__("Service Order: {0} submitted successfully.", [doc.name]), 5);
+                    } else {
+                        frappe.show_alert(__("Failed to submit Service Order {0}.", [doc.name]), 5);
+                    }
+                }
+            });
+        },
+    },
 };
 
 
-var service_order_show_dialog = (listview) => {
+var show_dialog = (listview) => {
     let d = new frappe.ui.Dialog({
         title: "Bulk Service Order",
         fields: [
@@ -44,48 +71,6 @@ var service_order_show_dialog = (listview) => {
                                 indicator: 'green'
                             }, 10);
                             listview.refresh();
-                        }
-                    }
-                });
-            }
-        }
-    });
-
-    d.show();
-}
-
-
-var sales_order_show_dialog = () => {
-    let d = new frappe.ui.Dialog({
-        title: "Create Sales Order",
-        fields: [
-            {
-                label: 'M BL No',
-                fieldname: 'm_bl_no',
-                fieldtype: 'Data',
-                reqd: 1
-            },
-        ],
-        size: "small",
-        primary_action_label: 'Create Order',
-        primary_action(values) {
-            if (values) {
-                frappe.call({
-                    method: 'icd_tz.icd_tz.doctype.service_order.service_order.create_sales_order',
-                    args: {
-                        data: values
-                    },
-                    freeze: true,
-                    freeze_message: __('<i class="fa fa-spinner fa-spin fa-4x"></i>'),
-                    callback: (r) => {
-                        if (r.message) {
-                            d.hide()
-                            frappe.show_alert({
-                                message: __("Sales Order Created successfully"),
-                                indicator: 'green'
-                            }, 10);
-
-                            frappe.set_route("Form", "Sales Order", r.message);
                         }
                     }
                 });
