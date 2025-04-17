f1422b8859de8ce0f7ed64c3791cec561ac66d2a
av-dev2 <emgani@aakvatech.com>
2025-04-17 19:09:51 +0300

    feat: update freight indicator retrieval in get_manifest_details function

--- icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.py
+++ icd_tz/icd_tz/doctype/container_movement_order/container_movement_order.py
@@ -138,7 +138,7 @@ def get_manifest_details(manifest, m_bl_no=None):
             cd.container_no,
             cd.m_bl_no,
             cd.container_size,
-			cd.freight_indicator.as_("status"),
+			cd.freight_indicator,
             mb.cargo_classification.as_("cargo_type")
         )
         .where(
